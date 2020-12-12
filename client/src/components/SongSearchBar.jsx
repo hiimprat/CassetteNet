93a2ea0a174373716fe4a8c39f99219df676e29f
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-12-12 02:16:16 -0500

    use a different (non buggy) autocomplete component

--- client/src/components/SongSearchBar.jsx
+++ client/src/components/SongSearchBar.jsx
@@ -1,67 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { songSearch } from '../utils/api';
 import { debounce } from 'lodash';
 import { CircularProgress, TextField } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
+import Autocomplete from 'react-autocomplete';
 
 
-function SongSearchBar(props) {
-  const [open, setOpen] = useState(false);
+function SongSearchBar({ apiToUse, setSelected, toExclude }) {
+  const [loading, setLoading] = useState(false);
   const [options, setOptions] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  const { apiToUse, setSelected, toExclude } = props;
 
   useEffect(() => {
-    if (!searchQuery) return; // don't bother calling youtube api if search is empty
+    if (!searchQuery) return; // don't bother calling api if search is empty
     setLoading(true); // make loading circle appear
     songSearch(apiToUse, searchQuery)
-      .then(res => {
-        setOptions(res.filter(s => !toExclude.includes(s.id)));
+      .then(results => {
+        setOptions(results.filter(s => !toExclude.includes(s.id)));
         setLoading(false);
-        console.log(res)
       })
       .catch(err => alert(err));
   }, [searchQuery]);
 
   const search = (e) => {
-    if (e.target.value) {
-      setSearchQuery(e.target.value);
-    }
+    setSearchQuery(e.target.value);
   }
 
   return (
     <Autocomplete
-      // style={{ width: 300 }}
-      open={open}
-      onOpen={() => setOpen(true)}
-      onClose={() => setOpen(false)}
-      onChange={(option, value) => setSelected(value)}
-      getOptionLabel={option => option.name}
-      getOptionSelected={(option, value) => option.id === value.id}
-      options={options}
-      loading={loading}
-      clearOnBlur={false}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          onKeyPress={() => setLoading(true)}
-          onBlur={() => setLoading(false)}
-          onChange={debounce((e) => search(e), 500)} // debounce every 700ms to prevent calling api more than needed
-          label="Search"
-          variant="outlined"
-          InputProps={{
-            ...params.InputProps,
-            endAdornment: (
-              <React.Fragment>
-                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            ),
-          }}
-        />
+      getItemValue={item => item.name}
+      items={options}
+      value={searchQuery}
+      renderItem={(item, isHighlighted) => (
+        <div style={{cursor: 'pointer'}}>{item.name}</div>
       )}
+      onChange={search}
+      onSelect={(name, val) => setSelected(val)}
     />
   );
 }
